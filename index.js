53488f2be1c375b978be5ac7228da76f06d04c7e
Ethan Jaffe <jaffe413@gmail.com>
2020-04-02 18:49:40 +0300

    eslint fixes - automated eslint on commit

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const express = require('express');
+import express from 'express';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import jsonConfig from 'json-configurator-store';
 
 const port = 3000;
 const app = express();
 
-const jsonConfig = require('json-configurator-store');
-
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.post('/project/create', async (req, res) => {
-    const projectName = req.body.projectName;
+    const { projectName } = req.body;
     await jsonConfig.createProject(projectName);
     res.status(201).send(`Successfully created ${projectName}`);
 });
 
 app.get('/project/:projectName', async (req, res) => {
-    const projectName = req.params.projectName;
+    const { projectName } = req.params;
     const files = await jsonConfig.getProjectFiles(projectName);
     res.status(200).json(files);
 });
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
