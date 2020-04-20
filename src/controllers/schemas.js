18d434e76e930e5953d269d0ecb75ed24e0164dc
Ethan Jaffe <6400075+ETHANTALJAFFE@users.noreply.github.com>
2020-04-20 19:41:36 +0300

    added schemas.js request validation

--- src/controllers/schemas.js
+++ src/controllers/schemas.js
@@ -1,8 +1,9 @@
+import { check } from 'express-validator';
 import createError from 'http-errors';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import jsonConfig from 'json-configurator-store';
 
-const schemasController = {
+const requests = {
     getSchema: async (req, res, next) => {
         const { projectName, schemaName } = req.params;
         try {
@@ -23,6 +24,48 @@ const schemasController = {
             next(createError(404, err.message));
         }
     }
+};
+
+const validate = {
+    getSchema: [
+        check('projectName')
+            .exists()
+            .withMessage('Missing field')
+            .isString()
+            .withMessage('Field must be of type String'),
+        check('schemaName')
+            .exists()
+            .withMessage('Missing field')
+            .isString()
+            .withMessage('Field must be of type String'),
+    ],
+    createSchema: [
+        check('projectName')
+            .exists()
+            .withMessage('Missing Field')
+            .isString()
+            .withMessage('Field must be of type String'),
+        check('schemaName')
+            .exists()
+            .withMessage('Missing Field')
+            .isString()
+            .withMessage('Field must be of type String'),
+        check('schemaData')
+            .exists()
+            .withMessage('Missing Field')
+            .not()
+            .isString()
+            .withMessage('Field must be a JSON Object'),
+        check('suffix')
+            .exists()
+            .withMessage('Missing Field')
+            .isString()
+            .withMessage('Field must be of type String')
+    ]
+};
+const schemasController = {
+    requests,
+    validate
 };
 
 export default schemasController;
