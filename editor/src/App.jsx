6a73f97344acc77ef22221a2b98e42d6b2c83a41
Ethan Jaffe <6400075+ETHANTALJAFFE@users.noreply.github.com>
2020-04-27 18:40:59 +0300

    added mui theme toggling button in top bar

--- editor/src/App.jsx
+++ editor/src/App.jsx
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Projects } from './components/Projects';
-import { AppBar, Paper, Toolbar, Typography } from '@material-ui/core';
+import { Brightness7Sharp, Brightness4Sharp } from '@material-ui/icons';
+import { AppBar, Paper, Toolbar, Typography, Switch, IconButton } from '@material-ui/core';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import lightTheme from './themes/light.json';
 import darkTheme from './themes/dark.json';
 import { ThemedContainer, ThemedPageBackground } from './components/themed';
 
 function App() {
-    const theme = createMuiTheme(darkTheme);
+    const [shouldUseDarkTheme, setShouldUseDarkTheme] = useState(false);
+
+    const theme = createMuiTheme(shouldUseDarkTheme ? darkTheme : lightTheme);
+    const toggleTheme = () => setShouldUseDarkTheme(!shouldUseDarkTheme);
+    const ThemeToggleIcon = () => (shouldUseDarkTheme ? <Brightness7Sharp /> : <Brightness4Sharp />);
+
     return (
         <ThemeProvider theme={theme}>
             <ThemedPageBackground fixed elevation={0} square>
                 <AppBar position="static">
-                    <Toolbar>
+                    <Toolbar style={{ display: 'flex', justifyContent: 'space-between' }}>
                         <Typography variant="h6">JSON Configuration Service</Typography>
+                        <div>
+                            <IconButton onClick={toggleTheme} color="inherit">
+                                <ThemeToggleIcon />
+                            </IconButton>
+                        </div>
                     </Toolbar>
                 </AppBar>
                 <Projects />
