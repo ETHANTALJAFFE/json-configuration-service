7dc241809cfd9b1806ac71d1181a8e847bcb9dde
Ethan Jaffe <6400075+ETHANTALJAFFE@users.noreply.github.com>
2020-04-27 12:49:32 +0300

    added editor directory to project - installed react in editor, added prettier for code linting

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import cors from 'cors';
+import path from 'path';
 import YAML from 'yamljs';
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
@@ -6,12 +7,13 @@ import projectsRouter from './src/routes/projects';
 
 const swaggerDocument = YAML.load('./swagger.yaml');
 
-const port = 8080;
+const port = 8080 || process.env.PORT;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../editor/build')));
 
 app.use('/projects', projectsRouter);
 
@@ -20,6 +22,15 @@ app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use((err, req, res, next) => {
     res.status(err.status).send(err);
     next();
+});
+
+app.get('/', (req, res, next) => {
+    res.status(200).send('Welcome to the JSON Configuration Service!\n To see the editor, change the path to /editor');
+    next();
+});
+
+app.use('/editor', function (req, res) {
+    res.sendFile(path.join(__dirname, '../editor/build', 'index.html'));
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
